0dd5c7af1cb83a9932e3024f18b37870bedbe79e
Sanjay <128044362+sbheemar@users.noreply.github.com>
2024-07-10 17:57:43 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity5.java
@@ -0,0 +1,42 @@
+package demos;
+
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity5 {
+    public static void main(String[] args) {
+        WebDriverManager.firefoxdriver().setup();
+        
+        WebDriver driver = new FirefoxDriver();
+        
+        Actions builder = new Actions(driver);
+
+        // Open the Training Support page
+        driver.get("https://v1.training-support.net/selenium/input-events");
+        // Print the title of the page
+        System.out.println("Home page title: " + driver.getTitle());
+
+        // Action on Left click
+        builder.click().pause(1000).build().perform();
+        // Print the front side text
+        String frontText = driver.findElement(By.className("active")).getText();
+        System.out.println(frontText);
+
+        // Action on Double click
+        builder.doubleClick().pause(1000).build().perform();
+        // Print the front side text
+        frontText = driver.findElement(By.className("active")).getText();
+        System.out.println(frontText);
+
+        // Action on right click
+        builder.contextClick().pause(1000).build().perform();
+        // Print the front side text
+        frontText = driver.findElement(By.className("active")).getText();
+        System.out.println(frontText);
+
+        driver.close();
+    }
+}
